067022bbf72951376c81e921dff58266126b30bd
JerryWski <jarek_wasilewski@yahoo.com>
2022-10-20 19:40:08 +0200

    js cd

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,23 @@
-const submitBtn = document.querySelector('#submit-button');
-const rateBtn = document.querySelector('#rate-button');
-const ratedInputs = document.querySelectorAll('.card__numbers--noactive');
-const activeInput = document.querySelector('.active');
-const checkedRating = document.querySelector('#checked');
-const thankYouCard = document.querySelector('.thank-you')
-const mainCard = document.querySelector('.card');
-console.log(mainCard);
+const submitBtn = document.querySelector("#submit-button");
+const rateBtn = document.querySelector("#rate-button");
+const ratedInputs = document.querySelectorAll(".card__numbers--noactive");
+const activeInput = document.querySelector(".active");
+const checkedRating = document.querySelector("#checked");
+const thankYouCard = document.querySelector(".thank-you");
+const mainCard = document.querySelector(".card");
 
+ratedInputs.forEach((rated) => {
+  rated.addEventListener("click", () => {
+    checkedRating.innerHTML = rated.value;
+  });
+});
 
-submitBtn.addEventListener('click', () => {
-    mainCard.style.display='none';
-    thankYouCard.style.display='block';
-})
+submitBtn.addEventListener("click", () => {
+  mainCard.style.display = "none";
+  thankYouCard.style.display = "block";
+});
 
-rateBtn.addEventListener('click', () => {
-    mainCard.style.display='block';
-    thankYouCard.style.display='none';
-})
+rateBtn.addEventListener("click", () => {
+  mainCard.style.display = "block";
+  thankYouCard.style.display = "none";
+});
