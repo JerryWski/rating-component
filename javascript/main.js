2ff476dabebb05b6fd0d7572e99a18ac4b234300
JerryWski <jarek_wasilewski@yahoo.com>
2022-10-20 19:10:07 +0200

    js part

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+const submitBtn = document.querySelector('.card__button');
+const ratedInputs = document.querySelectorAll('.card__numbers--noactive');
+const activeInput = document.querySelector('.active');
+const checkedRating = document.querySelector('#checked');
+console.log(checkedRating);
